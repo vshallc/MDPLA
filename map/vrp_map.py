b106ee384aecbe70c0c5ffa9ef9d7329b55c825d
Xiaoxi Wang <vshallc@gmail.com>
2015-03-19 07:37:36 +0800

    pdf approximation function

--- map/vrp_map.py
+++ map/vrp_map.py
@@ -1,15 +1,15 @@
 
-def link_two_nodes(node1, node2):
-    node1.add_neighbours(node2)
-    node2.add_neighbours(node1)
+def link_two_nodes(node1, node2, travel_cost1, travel_cost2):
+    node1.add_neighbours(node2, travel_cost1)
+    node2.add_neighbours(node1, travel_cost2)
 
 
 class Node(object):
     def __init__(self):
-        self.neighbours = []
+        self.neighbours = dict()
 
-    def add_neighbour(self, neighbour):
-        self.neighbours.append(neighbour)
+    def add_neighbour(self, neighbour, travel_cost):
+        self.neighbours[neighbour] = travel_cost
 
 class Map(object):
     def __init__(self, road_map, task_set):
---
+++ utils/math_tools.py
@@ -0,0 +1,1 @@
+def norm_pdf_linear_approximation(mu, sigma):
